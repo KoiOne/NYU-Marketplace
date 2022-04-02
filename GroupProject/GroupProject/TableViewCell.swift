4765144cb6f26ce68213ee9df23d96433da797f6
Xingyu Wang <44791063+KoiOne@users.noreply.github.com>
2022-04-02 12:49:11 -0400

    update

---
+++ GroupProject/GroupProject/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  GroupProject
+//
+//  Created by Xingyu Wang on 4/2/22.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
